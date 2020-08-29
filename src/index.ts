44d468d7cd3a21c0be9344203d615a7e110abac4
Alex <alex.trindade@live.com>
2020-08-29 15:52:27 -0300

    alterações para fazer o deploy na AWS

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,11 @@ import dotenv from "dotenv";
 import { AddressInfo } from "net";
 import express from "express";
 import { userRouter } from "./routes/userRouter";
+import cors from "cors";
 
 dotenv.config();
-const app = express();
-
+export const app = express();
+app.use(cors({ origin: true }));
 app.use(express.json());
 
 app.use("/user", userRouter);
